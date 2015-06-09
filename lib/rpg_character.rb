0b908be210d990ce6a5f41cd606f9837b520b6fd
itgsod-Alexander-Iglebk <Alexander.iglebk@elev.it-gymnasiet.se>
2015-06-09 13:09:30 +0200

    commit

---
+++ lib/rpg_character.rb
@@ -0,0 +1,9 @@
+def create_character(first_name: ,last_name: , klass: ,hitpoints: ,weapon:)
+    character = {first_name:first_name , last_name:last_name , klass:klass , hitpoints:hitpoints, weapon:weapon}
+    return character
+end
+x = create_character(first_name: "Thurgar", last_name: "Baneswolfe", klass: "Cleric", hitpoints: "109", weapon: "Quarterstaff")
+
+def print_format_character(character:)
+
+end
